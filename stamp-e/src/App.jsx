59a17054c2877ed5c2b6bcfa0fc3f9f126c22879
Selena Nguyen <snguyen3320@hwdsb.on.ca>
2025-05-27 09:54:27 -0400

    add card

--- stamp-e/src/App.jsx
+++ stamp-e/src/App.jsx
@@ -3,21 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const Card = ({villager}) => {
+const Orange = () => {
   return (
     <div>
-      <h2>{villager}</h2>
+      <img src="Orange.svg"></img>
     </div>
   )
 }
 
+
+const Start = () => {
+  return (
+    <div class="bg-red-balloon px-4 py-2 rounded-lg flex items-center justify-center">
+      <button type="button">Play</button>
+      <h1>hello</h1>
+    </div>
+  )   
+  
+}
 const App = () => {
   return (
     <div class="bg-bell-yellow h-screen flex flex-col items-center justify-center">
-      <h1 className="text-Sour Gummy">
-        Stampe
-      </h1>
-
+      <Orange/> 
     </div>
   )
 }
